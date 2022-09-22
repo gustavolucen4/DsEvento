e406d4bb842b38a6403b0c309ec9f324f01f04c2
Gustavo Lucena <gustavolucena12@gmail.com>
2022-09-22 09:22:19 -0300

    Creating project

---
+++ src/main/java/com/devsuperior/dsevendo/DsevendoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsevendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsevendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsevendoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dsevendo/DsevendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsevendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsevendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
